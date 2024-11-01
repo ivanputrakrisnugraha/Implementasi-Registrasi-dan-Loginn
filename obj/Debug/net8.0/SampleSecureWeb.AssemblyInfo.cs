c9403bbdf3748b65036d7e1e4d8a7265c3f58567
ivanputrakrisnugraha <ivanbaru321@gmail.com>
2024-11-01 19:14:28 +0700

    Upload Project

--- obj/Debug/net8.0/SampleSecureWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/SampleSecureWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleSecureWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9cd1ed33fdffd2055333032e46a221f9f3f9a00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1df1b1fc2d3dcbeaed4f802f1471a4756c425d08")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleSecureWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleSecureWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
